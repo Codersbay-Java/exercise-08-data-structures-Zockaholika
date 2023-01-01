856dbf8d599d41e73c02eacd7722504b90845864
Tobias Bichler <113764832+Zockaholika@users.noreply.github.com>
2023-01-01 20:33:16 +0100

    Part 1 finished

--- src/main/java/com/codersbay/part1/Main.java
+++ src/main/java/com/codersbay/part1/Main.java
@@ -3,6 +3,19 @@ package com.codersbay.part1;
 public class Main {
     public static void main(String[] args) {
         PhoneBook phoneBook = new PhoneBook();
+        phoneBook.insert("Tobias","Bichler", 1);
+        //System.out.println(phoneBook);
+        phoneBook.insert("Tobias","Bichler", 2);
+        phoneBook.insert("Tobias","Bichler", 3);
+        //System.out.println(phoneBook);
+        phoneBook.insert("Tobias","Bichler", 4);
+        phoneBook.insert("Tobias","Bichler", 5);
+        //System.out.println(phoneBook);
+
+        //phoneBook.print();
+        phoneBook.remove(1);
+        System.out.println(phoneBook);
+        phoneBook.update(1,"Tobias","Bichler", 99);
         System.out.println(phoneBook);
     }
 }
--- src/main/java/com/codersbay/part1/Person.java
+++ src/main/java/com/codersbay/part1/Person.java
@@ -1,4 +1,22 @@
 package com.codersbay.part1;
 
 public class Person {
+    String firstName;
+    String sureName;
+    int telephoneNumber;
+
+    public Person(String firstName, String sureName, int telephoneNumber) {
+        this.firstName = firstName;
+        this.sureName = sureName;
+        this.telephoneNumber = telephoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "Firstname='" + firstName + '\'' +
+                ", Surname='" + sureName + '\'' +
+                ", Telephone number=" + telephoneNumber +
+                '}';
+    }
 }
